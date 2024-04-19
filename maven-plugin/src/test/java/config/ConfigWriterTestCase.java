1f730366f6719de94bbff1770cc365a8df85d5ca
Tim Carter <tim.e.carter94@gmail.com>
2024-04-19 16:35:48 +1000

    Create initial maven plugin

---
+++ maven-plugin/src/test/java/config/ConfigWriterTestCase.java
@@ -0,0 +1,64 @@
+package config;
+
+import dev.sashimono.mavenplugin.config.ConfigWriter;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.apache.maven.project.MavenProject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class ConfigWriterTestCase {
+
+    @Test
+    public void testMultiModuleConfigWriter(@TempDir final File tempDir) throws IOException {
+        final Model model = new Model();
+        model.setGroupId("com.acme");
+        model.setArtifactId("parent");
+        model.setVersion("1.0");
+        model.setPackaging("pom");
+        model.setModules(List.of("foo", "bar"));
+        final MavenProject project = new MavenProject(model);
+        project.setFile(tempDir.toPath().resolve("pom.xml").toFile());
+        final Dependency dependency = new Dependency();
+        dependency.setGroupId("org.apache.httpcomponents");
+        dependency.setArtifactId("httpclient");
+        dependency.setVersion("4.5.14");
+        dependency.setScope("compile");
+        project.setDependencies(List.of(dependency));
+        ConfigWriter.writeConfig(project);
+        final String fileContents = Files
+                .readString(tempDir.toPath().resolve(ConfigWriter.SASHIMONO_DIR).resolve(ConfigWriter.DEPENDENCIES_LIST));
+        Assertions.assertEquals(
+                "artifact com.acme:parent:1.0\npackaging pom\nmodule foo\nmodule bar\nrequire org.apache.httpcomponents:httpclient:4.5.14\n",
+                fileContents);
+    }
+
+    @Test
+    public void testSingleModuleConfigWriter(@TempDir final File tempDir) throws IOException {
+        final Model model = new Model();
+        model.setGroupId("com.acme");
+        model.setArtifactId("foo");
+        model.setVersion("1.0");
+        model.setPackaging("jar");
+        final MavenProject project = new MavenProject(model);
+        project.setFile(tempDir.toPath().resolve("pom.xml").toFile());
+        final Dependency dependency = new Dependency();
+        dependency.setGroupId("org.apache.httpcomponents");
+        dependency.setArtifactId("httpclient");
+        dependency.setVersion("4.5.14");
+        dependency.setScope("compile");
+        project.setDependencies(List.of(dependency));
+        ConfigWriter.writeConfig(project);
+        final String fileContents = Files
+                .readString(tempDir.toPath().resolve(ConfigWriter.SASHIMONO_DIR).resolve(ConfigWriter.DEPENDENCIES_LIST));
+        Assertions.assertEquals("artifact com.acme:foo:1.0\npackaging jar\nrequire org.apache.httpcomponents:httpclient:4.5.14\n",
+                fileContents);
+    }
+
+}
